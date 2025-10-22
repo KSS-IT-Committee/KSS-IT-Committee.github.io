89c8c89a4bc9056a4e4c2e3caef4594a164bd191
rotarymars <s.goto2050@gmail.com>
2025-10-22 13:10:23 +0900

    Update logo image source path in Home component
    Sorry for messing around

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import itLogo from "/IT-logo.png";
 
 export default function Home() {
   return (
     <>
     <h1>IT Committee Demo Home page!</h1>
     <Image
-      src={itLogo}
+      src="../../public/IT-logo.png"
       width={200}
       height={200}
       alt="IT Committee logo"
