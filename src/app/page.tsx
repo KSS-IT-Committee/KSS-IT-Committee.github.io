5602d7380d60e6c5f8cac8cf935775a11993a2f1
rotarymars <s.goto2050@gmail.com>
2025-10-30 15:42:13 +0900

    Translated page to Japanese

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-    <h1 className={styles.h1}>KSS IT Committee Demo Home page!</h1>
+    <h1 className={styles.h1}>都立小石川中等教育学校　IT委員会</h1>
     <Image
       src="/images/IT-logo.png"
       width={200}
       height={200}
-      alt="IT Committee logo"
+      alt="IT委員会のロゴ"
       ></Image>
     </>
   );
