09a7e3f95b8cfef0aff28dcb96b12687012f0b18
rotarymars <s.goto2050@gmail.com>
2026-01-02 17:23:14 +0900

    created demo page

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,13 @@
+import styles from "@/styles/base.module.css";
+import DynamicLink from "@/components/DynamicLink";
+
+export default function Page() {
+  return (
+    <>
+    <h1>デモページ一覧</h1>
+    <ul>
+      <li><DynamicLink link="/demo/rotarymars.nolink">rotarymars</DynamicLink></li>
+    </ul>
+    </>
+  );
+}
