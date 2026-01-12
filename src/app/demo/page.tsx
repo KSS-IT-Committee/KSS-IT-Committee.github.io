3692c54ea589224efb2a51301deaab4b955b0450
K10-K10 <keito.tottori@gmail.com>
2026-01-12 23:11:58 +0900

    comment out unused 'styles'

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,14 +1,14 @@
-import styles from "@/styles/base.module.css";
+// import styles from "@/styles/base.module.css";
 import DynamicLink from "@/components/DynamicLink";
 
 export default function Page() {
   return (
     <>
-    <h1>デモページ一覧</h1>
-    <ul>
-      <li><DynamicLink link="/demo/rotarymars.nolink">rotarymars</DynamicLink></li>
-      <li><DynamicLink link="/demo/utsukushiioto0816-tech">utsukushiioto0816-tech</DynamicLink></li>
-    </ul>
+      <h1>デモページ一覧</h1>
+      <ul>
+        <li><DynamicLink link="/demo/rotarymars.nolink">rotarymars</DynamicLink></li>
+        <li><DynamicLink link="/demo/utsukushiioto0816-tech">utsukushiioto0816-tech</DynamicLink></li>
+      </ul>
     </>
   );
 }
